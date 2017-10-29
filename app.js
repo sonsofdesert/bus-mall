54d02fe1e6c00af600dad2635627091f73d418bf
Eric Cobb <eric.cobb2017@gmail.com>
2017-10-28 18:03:54 -0700

    added pics in pic file, comepleted user story, started js

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var allPics = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+var ViewImage = 0;
+
+var Clicks = 0;
+
+function randomNumber() {
+  return Math.floor(Math.random() * 20);
+}
